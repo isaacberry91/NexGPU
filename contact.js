37a4d2b4afdb46097b128d9de99626159c3c5413
isaacberry91 <isaacberry91@yahoo.com>
2025-04-21 19:06:10 -0400

    Delete contact.js

--- contact.js
+++
@@ -1,8 +0,0 @@
-export default function Contact() {
-  return (
-    <div style={{ fontFamily: 'Arial', padding: '2rem' }}>
-      <h1>Contact Us</h1>
-      <p>Need help? Reach us at support@nexgpu.com or fill out the form below.</p>
-    </div>
-  );
-}
