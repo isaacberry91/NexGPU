e69be3d50c22ac61d51dd9bcab6db795a1202c2f
isaacberry91 <isaacberry91@yahoo.com>
2025-04-21 19:05:29 -0400

    Delete about.js

--- about.js
+++
@@ -1,8 +0,0 @@
-export default function About() {
-  return (
-    <div style={{ fontFamily: 'Arial', padding: '2rem' }}>
-      <h1>About NexGPU</h1>
-      <p>NexGPU is your go-to platform for flexible, on-demand GPU and cloud compute rentals. Built for developers, researchers, and creators.</p>
-    </div>
-  );
-}
