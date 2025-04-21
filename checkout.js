401f3b9c727de4224d802f0d19869bee8fd37539
isaacberry91 <isaacberry91@yahoo.com>
2025-04-21 19:05:41 -0400

    Delete checkout.js

--- checkout.js
+++
@@ -1,9 +0,0 @@
-export default function Checkout() {
-  return (
-    <div style={{ fontFamily: 'Arial', padding: '2rem' }}>
-      <h1>Checkout</h1>
-      <p>Your GPU is ready to deploy. Proceed with payment.</p>
-      <button style={{ marginTop: '1rem', padding: '0.5rem 1rem' }}>Pay with Stripe</button>
-    </div>
-  );
-}
