3e9adc9a5fc4cf96ea911fc63229c0f45124d9c0
isaacberry91 <isaacberry91@yahoo.com>
2025-04-21 10:16:00 -0600

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>Welcome to NexGPU</h1>
+      <p>Rent cloud GPUs and servers by the hour.</p>
+    </div>
+  );
+}
