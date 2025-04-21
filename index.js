29d25cc89373b38915351cba530eab6ce489a044
isaacberry91 <isaacberry91@yahoo.com>
2025-04-21 19:07:09 -0400

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ fontFamily: 'Arial', padding: '2rem' }}>
-      <h1>Welcome to NexGPU</h1>
-      <p>Rent powerful GPUs and cloud systems by the hour.</p>
-      <a href="/checkout"><button style={{ marginTop: '1rem', padding: '0.5rem 1rem' }}>Rent Now</button></a>
-    </div>
-  );
-}
